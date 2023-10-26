b8a98d8c49614343fb51e85db0d3f622844121b9
Acer1 <Acer1@Acer>
2023-10-26 12:07:39 -0500

    Primer commit

---
+++ selenium/src/test/java/practicaSel/PruebaSeleniumTest.java
@@ -0,0 +1,14 @@
+package practicaSel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PruebaSeleniumTest {
+  @Test
+  public void f() {
+	  WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+  }
+}
